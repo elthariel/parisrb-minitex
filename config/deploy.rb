07906576ca79da0a1b2b123e5bcc39de99de33ba
Julien 'Lta' BALLET <contact@lta.io>
2013-06-04 12:25:45 +0200

    Adds thin to capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,17 +26,21 @@ require "bundler/capistrano"
 
 before 'deploy:setup', 'rvm:install_rvm', 'rvm:install_ruby'
 
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+# Generate an additional task to fire up the thin clusters
+namespace :deploy do
+  desc "Start the Thin processes"
+  task :start do
+    sudo "bundle exec thin start -C config/thin.yml"
+  end
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+  desc "Stop the Thin processes"
+  task :stop do
+    sudo "bundle exec thin stop -C config/thin.yml"
+  end
+
+  desc "Restart the Thin processes"
+  task :restart do
+    sudo "bundle exec thin restart -C config/thin.yml"
+  end
+end
